6b96bf1c5516a93c34c1031a7b371dddfdf9ac25
suhanyujie <suhanyujie@qq.com>
2016-07-29 09:17:24 +0800

    storage data by redis (predis)

--- App/NovelSpider/Controller/Test.php
+++ App/NovelSpider/Controller/Test.php
@@ -3,16 +3,46 @@
 namespace Novel\NovelSpider\Controller;
 
 use QL\QueryList;
+use Predis\Client;
 
 class Test{
+    // 只针对 "大宋王候" 的Novel
+    protected $baseUrl = 'http://www.biquwu.cc/biquge/17_17308/';
+    protected $redisObj = null;
+
+    public function __construct(){
+        if(!$this->redisObj){
+            $this->redisObj = new \Predis\Client();
+        }
+    }
 
     public function getList(){
-        $hj = QueryList::Query('http://www.biquwu.cc/biquge/17_17308/',array("list"=>array('.article_texttitleb li:first','html')),'body','utf-8');
+        $hj = QueryList::Query('http://www.biquwu.cc/biquge/17_17308/',
+                                array(
+                                    "latest"=>array('.article_texttitleb li:last','html'),
+                                    "list"=>array('.article_texttitleb li','html'),
+
+                                ),
+                            'body','utf-8');
         $data = $hj->getData(function($x){
             return $x['list'];
         });
-        print_r($data);
+        //print_r($data);
         return $data;
+    }
+
+    /**
+     * 从redis获取列表
+     * @param string $cacheKey redis的key
+     * @return array|bool 返回数据列表
+     */
+    public function getListFromRedis($cacheKey=''){
+        if(!$cacheKey)return false;
+        $redis = $this->redisObj;
+        //$redis -> hmset ( $keyConfig['list-key'] , $list ) ;
+        //$redis -> del ( $keyConfig['list-key'] ) ;
+        $res = $redis->hgetall($cacheKey);
+        return $res;
     }
 
 
--- App/NovelSpider/start.php
+++ App/NovelSpider/start.php
@@ -6,21 +6,36 @@ use \Workerman\Lib\Timer;
 
 use QL\QueryList;
 use Novel\NovelSpider\Controller\Test;
+use Predis\Client;
 
 $task = new Worker();
 // 开启多少个进程运行定时任务，注意多进程并发问题
 $task->count = 1;
 $task->onWorkerStart = function($task) {
-    // 获取列表
-    $novel = new Test();
-    $novel->getList();
-    // 获取详情
+    $keyConfig = [
+        'list-key'=>'novel-list-key',
+        'detail-key'=>'novel-detail-key',
+    ];
+    // 只在id编号为0的进程上设置定时器，其它1、2、3号进程不设置定时器
+    if($task->id === 0){
+        // 获取列表
+        $novel = new Test();
+        //$list = $novel->getList();
+        //$redis = new Predis\Client();
+        // 将列表存进redis存放
+        // 从redis取出数据
+        //$res = $redis->hgetall($keyConfig['list-key']);
+        $res = $novel->getListFromRedis($keyConfig['list-key']);
 
-    // 定时请求,保证获取最新
-    $time_interval = 3;
-    Timer::add($time_interval, function(){
-        echo "task run\n";
-    });
+        var_dump($res);
+
+        // 读取redis队列,去爬取详情
+        // 定时请求,保证获取最新
+        $time_interval = 3;
+        Timer::add($time_interval, function(){
+            echo "task run\n";
+        });
+    }// end of if
 
 };
 
