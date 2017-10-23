65912d55809ca0cbb0142ada6fdefd5865623a42
suhanyujie <suhanyujie@qq.com>
2017-10-23 08:43:14 +0800

    1.调整注释 2.爬取小说内容的类文件

---
+++ App/NovelSpider/Controller/ContentSpider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 17/10/23
+ * Time: 上午8:40
+ */
+
+namespace Novel\NovelSpider\Controller;
+
+use QL\QueryList;
+
+class ContentSpider
+{
+    /**
+     * @desc 通过详情页的url，获取小说内容
+     * @param $url
+     * @return string
+     */
+    public function getContent($url)
+    {
+        
+        return '';
+    }
+}
