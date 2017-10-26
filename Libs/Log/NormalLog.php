e0ffcef956cf42740bf6248d1b4ff4d89b4492b2
suhanyujie <suhanyujie@qq.com>
2017-10-26 23:14:24 +0800

    1.引入正确的文件处理类

--- Libs/Log/NormalLog.php
+++ Libs/Log/NormalLog.php
@@ -8,23 +8,49 @@
 
 namespace Libs\Log;
 
+use Libs\Helper\PublicFunction;
+use Libs\Helper\FileFunction;
+
 
 class NormalLog
 {
     /**
      * @desc 构造函数
+     *  1.获取日志文件名
+     *  2.判断文件是否存在，不存在，则创建
+     *  3.将日志内容写入文件当中
+     *
      * NormalLog constructor.
+     * @param string $businessName 日志业务名，如果不存在，则新建文件夹
      */
-    public function __construct()
+    public function __construct($businessName='')
     {
+        $fileName = $this->getFileName();
+        // 创建文件夹
+        FileFunction::mkDirs(dirname($fileName));
+    }
     
+    /**
+     * @desc 获取日志文件名 全路径
+     * @param string $businessName
+     * @return string
+     */
+    public function getFileName($businessName = 'NormalLog')
+    {
+        $dateEleArr = [
+            date('Y'),
+            date('m'),
+        ];
+        $fileName = PublicFunction::storage_path() . '/' . $businessName . '/' . implode('/', $dateEleArr) . '/' . date('d') . '.log';
+        
+        return $fileName;
     }
     
     /**
      * @desc 通过当前日期生成日志文件
      */
-    public function getLogFile()
+    public function getDateString()
     {
-    
+        return date('Y-m-d');
     }
 }
