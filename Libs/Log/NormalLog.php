1a62b7bbced9907cee796bd6fb6e3dc30c283ffc
suhanyujie <suhanyujie@qq.com>
2017-10-26 22:27:23 +0800

    1.新增通用日志文件

---
+++ Libs/Log/NormalLog.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 17/10/26
+ * Time: 下午10:06
+ */
+
+namespace Libs\Log;
+
+
+class NormalLog
+{
+    /**
+     * @desc 构造函数
+     * NormalLog constructor.
+     */
+    public function __construct()
+    {
+    
+    }
+    
+    /**
+     * @desc 通过当前日期生成日志文件
+     */
+    public function getLogFile()
+    {
+    
+    }
+}
