9597fd96c82f270631dbbaad3bc6b8345784d06c
suhanyujie <suhanyujie@qq.com>
2017-11-03 09:17:23 +0800

    1.简单测试

--- App/NovelSpider/test.php
+++ App/NovelSpider/test.php
@@ -3,6 +3,8 @@ require __DIR__."/../../vendor/autoload.php";
 
 use Novel\NovelSpider\Controller\ListSpider;
 use Novel\NovelSpider\Controller\ListStore;
+use Libs\Log\NormalLog;
+use Novel\NovelSpider\Services\SdealUpdate;
 
 $fictionList = [
     1 => [
@@ -27,6 +29,20 @@ $fictionList = [
 // 采集详情内容
 //$contentSpider = new \Novel\NovelSpider\Controller\ContentSpider();
 //$contentSpider->getContent();
+
+// 测试日志功能
+$logger = new NormalLog();
+$logger->info('测试-记录 信息');
+
+// 测试 更新小说连载
+if ($fictionList) {
+    foreach ( $fictionList as $k => $item ) {
+        $dealUpdateService = new SdealUpdate($k);
+        $dealUpdateService->getInternetUpdate();
+        break;
+    }
+}
+
 
 
 
