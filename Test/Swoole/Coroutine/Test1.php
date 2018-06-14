eb3bd0557dd57f820af3ccb7297a87ac40b562cd
suhanyujie <suhanyujie@qq.com>
2018-06-14 10:30:28 +0800

    coroutine的http客户端

--- Test/Swoole/Coroutine/Test1.php
+++ Test/Swoole/Coroutine/Test1.php
@@ -20,9 +20,9 @@ class Test1
     public function index()
     {
         $startTime = microtime(true);
-//        go([$this, 'getList']);
-        $this->data['testUrl'] = 'http://www.biquge.com.tw/14_14055/';
         go([$this, 'getList']);
+//        $this->data['testUrl'] = 'http://www.biquge.com.tw/14_14055/';
+//        go([$this, 'getList']);
 //        $this->data['testUrl'] = 'http://guzzle-cn.readthedocs.io/zh_CN/latest/quickstart.html';
 //        go([$this, 'getList']);
         $endTime = microtime(true);
@@ -41,18 +41,23 @@ class Test1
             $url = $this->data['testUrl'];
         }
         try{
-            $httpClient = new \GuzzleHttp\Client([
-                'timeout'  => 5,
-            ]);
-            $response = $httpClient->request('get',$url);
-            $response = $response->getBody();
+//            $httpClient = new \GuzzleHttp\Client([
+//                'timeout'  => 5,
+//            ]);
+//            $response = $httpClient->request('get',$url);
+//            $response = $response->getBody();
+            $cli = new \Swoole\Coroutine\Http\Client('https://www.swoole.com', 443);
+            $cli->set(['timeout'=>2,]);
+            $cli->get('/wiki/page/774.html');
+            $cli->recv();
+            $res = $cli->body;
+            $cli->close();
         }catch(\Exception $e){
             echo 'error ';
         }
-        var_dump((string)$response);
+        var_dump((string)$res);
     }
 }
-
 $event = new Test1();
 $event->index();
 
