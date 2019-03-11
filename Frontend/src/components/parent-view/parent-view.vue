98aae0fa643c7ceced667c31b51b6c0226de6205
suhanyujie <suhanyujie@qq.com>
2019-03-11 17:51:45 +0800

    删除前端解面目录 将其独立成仓库

--- Frontend/src/components/parent-view/parent-view.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <router-view/>
-</template>
-<script>
-export default {
-  name: 'ParentView'
-}
-</script>
---
+++ Novel/Controllers/Spider/MainListController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 2019/3/11
+ * Time: 14:57
+ */
+
+namespace Novel\Controllers\Spider;
+
+
+use Novel\Controllers\Controller;
+
+class MainListController extends Controller
+{
+
+}
