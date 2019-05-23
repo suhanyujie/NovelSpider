5f965fb271483f04c0fb34bdf84b6dfd9f1c5b94
suhanyujie <suhanyujie@qq.com>
2019-05-23 19:00:03 +0800

    添加 novel list service

---
+++ Novel/NovelSpider/Services/NovelListService.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 2019/5/23
+ * Time: 18:55
+ */
+
+namespace Novel\NovelSpider\Services;
+
+
+class NovelListService
+{
+    protected $listModel;
+
+    public function __construct()
+    {
+
+    }
+}
